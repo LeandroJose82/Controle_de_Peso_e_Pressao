0230809f914773272cf4b5dd5d3820a7de2f7209
leandrojose82 <leandrojcazevedo@gmail.com>
2021-12-11 04:26:02 -0300

    -Adicionado um Slider para selecionar o peso no lugar do TextView -Adicionado uma nova cor ao background das telas

--- app/src/main/java/lj/controledepesoepresso/activities/FormularioNovoPeso.kt
+++ app/src/main/java/lj/controledepesoepresso/activities/FormularioNovoPeso.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.slider.Slider
 import kotlinx.coroutines.runBlocking
 import lj.controledepesoepresso.R
 import lj.controledepesoepresso.database.ControleDatabase
@@ -17,24 +18,28 @@ class FormularioNovoPeso : AppCompatActivity() {
         setContentView(R.layout.activity_formulario_novo_peso)
 
         val btnHome: Button = findViewById(R.id.retornarHome)
-        val btnSalvarNovoPeso : Button = findViewById(R.id.salvarNovoPeso)
-        val txtEntradaNovoPeso : TextView = findViewById(R.id.entradaNovoPeso)
+        val btnSalvarNovoPeso: Button = findViewById(R.id.salvarNovoPeso)
+        val sliderPeso: Slider = findViewById(R.id.sliderPeso)
+        val txtResuldadoPeso: TextView = findViewById(R.id.resultadoPeso)
 
-        val db =   ControleDatabase.getDatabase(this)
+        val db = ControleDatabase.getDatabase(this)
+
+        sliderPeso.addOnChangeListener { slider, value, fromUser ->
+            // Responds to when slider's value is changed
+            txtResuldadoPeso.text = sliderPeso.value.toString()
+        }
 
         btnSalvarNovoPeso.setOnClickListener {
-           val verificarPeso =  txtEntradaNovoPeso.text
-            if (verificarPeso==null || verificarPeso.isEmpty()){
-                    Toast.makeText(this,R.string.insiraPeso,Toast.LENGTH_SHORT).show()
-            }else{
+            val verificarPeso = sliderPeso.value.toString()
+            if (verificarPeso.isEmpty()) {
+                Toast.makeText(this, R.string.insiraPeso, Toast.LENGTH_SHORT).show()
+            } else {
                 runBlocking {
-                    val novoPeso = Peso(peso=txtEntradaNovoPeso.text.toString().toDouble())
+                    val novoPeso = Peso(peso = verificarPeso.toDouble())
                     db.controleDAO().inserirPeso(novoPeso)
                 }
                 retornarHome()
             }
-
-
         }
 
         btnHome.setOnClickListener {
@@ -43,7 +48,6 @@ class FormularioNovoPeso : AppCompatActivity() {
 
 
     }
-
     private fun retornarHome() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
